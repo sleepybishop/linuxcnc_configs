d62ddbabaa8c53e0277a9597a9bfc3d8b65a9801
joseph calderon <calderon.joe@gmail.com>
2016-09-08 17:35:14 -0700

    refactor build system

--- mesa/source/vhd2prj.sh
+++ mesa/source/vhd2prj.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ]; then
 				exit -1
 fi
 
-echo "vhdl work $1.vhd" > $1.prj
+echo "vhdl work out/$1.vhd" > $1.prj
 for VHD in `find common/ -name '*.vhd'`; do echo vhdl work "$VHD" >> $1.prj ; done
 for VHD in `find card/   -name '*.vhd'`; do echo vhdl work "$VHD" >> $1.prj ; done
 for VHD in `find pin/    -name '*.vhd'`; do echo vhdl work "$VHD" >> $1.prj ; done
